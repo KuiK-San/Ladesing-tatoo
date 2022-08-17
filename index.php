bf1e0cb0718728400cc9a6d14287f1cec0f8681f
Guilherme Casagrande <thegui4000@gmail.com>
2022-08-17 12:36:04 -0300

    asd

--- index.php
+++ index.php
@@ -14,33 +14,9 @@
     <title>Ladesing - Tatuagem</title>
 </head>
 <body>
-    <div class="container-fluid p-0 user-select-none">
-        <div class="row-12">
-            <nav class="navbar fixed-top navbar-expand-lg navbar-dark d-flex align-items-center">
-                <a href="#" class="navbar-brand"> <img src="img/ladesign_headerlogo.png" alt="" width="60" height="60" class="d-inline-block align-top"></a>
-                <a href="#" class="navbar-brand ml-2">LADESING.TATOO</a>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                      <li class="nav-item active" >
-                        <a class="nav-link" href="#" id="navlink">Home</a>
-                      </li>
-                      <li class="nav-item" >
-                        <a class="nav-link" href="#" id="navlink">História</a>
-                      </li>
-                      <li class="nav-item" >
-                        <a class="nav-link" href="#" id="navlink">Alguns trabalhos</a>
-                      </li>
-                      <li class="nav-item" >
-                        <a class="nav-link" href="#" id="navlink">Flash Tatoos</a>
-                      </li>
-                      </li>
-                      <li class="nav-item" >
-                        <a class="nav-link" href="#" id="navlink">Agende um horário</a>
-                      </li>
-                    </ul>
-                  </div>
-            </nav>
-        </div>
+    <?php
+    include('modelos/header.php');
+    ?>
         <div class="row-12">
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
---
+++ modelos/header.php
@@ -0,0 +1,27 @@
+<div class="container-fluid p-0 user-select-none">
+        <div class="row-12">
+            <nav class="navbar fixed-top navbar-expand-lg navbar-dark d-flex align-items-center">
+                <a href="#" class="navbar-brand"> <img src="img/ladesign_headerlogo.png" alt="" width="60" height="60" class="d-inline-block align-top"></a>
+                <a href="#" class="navbar-brand ml-2">LADESING.TATOO</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                      <li class="nav-item active" >
+                        <a class="nav-link" href="#" id="navlink">Home</a>
+                      </li>
+                      <li class="nav-item" >
+                        <a class="nav-link" href="#" id="navlink">História</a>
+                      </li>
+                      <li class="nav-item" >
+                        <a class="nav-link" href="#" id="navlink">Alguns trabalhos</a>
+                      </li>
+                      <li class="nav-item" >
+                        <a class="nav-link" href="#" id="navlink">Flash Tatoos</a>
+                      </li>
+                      </li>
+                      <li class="nav-item" >
+                        <a class="nav-link" href="#" id="navlink">Agende um horário</a>
+                      </li>
+                    </ul>
+                  </div>
+            </nav>
+        </div>
