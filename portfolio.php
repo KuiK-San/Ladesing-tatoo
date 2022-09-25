ad2112aafa3e14686a14f761e6374d083d80a079
Guilherme Casagrande <thegui4000@gmail.com>
2022-09-24 22:20:41 -0300

    quase pronto

---
+++ portfolio.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    include('./modelos/imports.html');
+    ?>
+<!--     <meta http-equiv="refresh" content="3"> -->
+    <title>Portfólio - Ladesign</title>
+</head>
+<body>
+    <?php
+    include('./modelos/navbar.html');
+    ?>
+
+    <article class="historia grey darken-3"> 
+        <div class="container">
+            <div class="row">
+                <h3 class="col l12 white-text center-align">
+                    Alguns dos nossos trabalhos
+                </h3>
+            </div>
+            <div class="row">
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="./img/port/port.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="img/port/d2a58273f02dd92ec2acaf7587faabf4.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="img/port/c10a0f6727261d86d85b0ae63ecf9797.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="img/port/246f2eca851bb50e232367d02651d45d.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="img/port/3c46d4dfd557f19f5f029f97681ad686.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="img/port/0d74f4cc8fd2ad3ae92abd457cdb91f0.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="img/port/2ed7471e29a76fb6f2a38f62197fe083.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="img/port/92c3d921abf6255cc99da862427459e3.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="img/port/66b4cbd478ace792d200f4f3ba11977b.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="img/port/66b4cbd478ace792d200f4f3ba11977b.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+                <div class="port-section col l4 m6 s12">
+                    <div class="port-imagem center-align">
+                        <img class="responsive-img" src="img/port/3bba96fcc7cc62d60bc7abfa637f7fef.jpg" alt="">
+                    </div>
+                    <p class="center-align white-text flow-text">Tatuagem feita por: <br> Nome do tatuador</p>
+                </div>
+            </div>
+        </div>
+    </article>
+
+    <?php
+    include('./modelos/footer.html');
+    ?>
+</body>
+</html>
