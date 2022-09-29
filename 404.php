1bf53f1c9e9d350ec33228b4ef73668368a6d0b1
Guilherme Casagrande <thegui4000@gmail.com>
2022-09-29 15:52:37 -0300

    finalizado com detalhes

---
+++ 404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    include('modelos/imports.html');
+    ?>
+    <link rel="stylesheet" href=".css">
+    <title>404</title>
+</head>
+<body>
+    <?php
+    include('modelos/navbar.html');
+    ?>
+    <article class="historia h100 grey darken-4 valign-wrapper">
+        <div class="container">
+            <div class="">
+                <h1 class="center-align white-text">404</h1>
+                <h2 class="center-align white-text">Página não existente!</h2>
+            </div>
+        </div>
+    </article>
+    <?php
+    include('modelos/footer.html');
+    ?>  
+</body>
+</html>
